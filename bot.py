6338383d4b002260e79c58efd0861e5c05617cb6
Unknown <maxbrux@live.it>
2019-07-07 23:38:19 +0200

    Added delete message feature

--- bot.py
+++ bot.py
@@ -5,13 +5,18 @@ import re
 def trentaelode(bot, update):
     update.message.reply_text('30 e lode')
 
+def delete_message(bot, update):
+    bot.deleteMessage(chat_id=update.message.chat.id, message_id=update.message.message_id)
+
 def main():
     TOKEN = os.getenv("TOKEN")
     updater = Updater(TOKEN)
     
     dp = updater.dispatcher
-    pattern = re.compile('apice', re.IGNORECASE)
-    dp.add_handler(MessageHandler(Filters.regex(pattern), trentaelode))
+    pattern_apice = re.compile('apice', re.IGNORECASE)
+    dp.add_handler(MessageHandler(Filters.regex(pattern_apice), trentaelode))
+    pattern_max = re.compile('max.*3[0-9]', re.IGNORECASE)
+    dp.add_handler(MessageHandler(Filters.regex(pattern_max), delete_message))
 
     PORT = int(os.environ.get("PORT", "8443"))
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
